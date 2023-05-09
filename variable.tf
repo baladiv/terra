aedd00cd50793e8514acf70adfcaf341bdb351b5
Balamurugan P <ba20286698@wipro.com>
2023-05-09 20:37:56 +0530

    Add existing project files to Git

---
+++ variable.tf
@@ -0,0 +1,8 @@
+variable "access_key" {
+  default = "AKIAVXAWQJCD66KRGNEJ"
+  }
+variable "secret_key" {
+  default = "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"
+  }
+variable "region" {
+  default = "ap-south-1"
