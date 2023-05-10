7e71ad856cc2adc0046be41605ca1188cb2a5836
baladiv <111218535+baladiv@users.noreply.github.com>
2023-05-10 12:47:00 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,9 +1,9 @@
 resource "aws_s3_bucket" "bucklebuck" {
-  bucket = "my-tf-massiveprince-bucket11"
+  bucket = "bhashvika mirna"
   acl    = "private"
 
   tags = {
-    Name        = "rav1i-day1a-santhos1h-deepak1231"
-    Environment = "Dev"
+    Name        = "bhashvi bala"
+    Environment = "UAT"
   }
 }
