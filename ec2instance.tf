83e7fccef44843ba5885a6986ebd2277a7f01914
Balamurugan P <ba20286698@wipro.com>
2023-05-10 12:59:32 +0530

    t

---
+++ ec2instance.tf
@@ -0,0 +1,72 @@
+variable "awsprops" {
+    default = {
+    region = "ap-south-1"
+    vpc = "vpc-0225b695194accf68"
+    ami = "ami-052f483c20fa1351a"
+    itype = "t2.micro"
+    subnet = "subnet-096095a99f420edf2"
+    publicip = true
+    keyname = "terraform"
+    secgroupname = "IAC-Sec-Group098"
+  }
+}
+
+provider "aws" {
+  region = lookup(var.awsprops, "region")
+}
+
+resource "aws_security_group" "project-iac-sg" {
+  name = lookup(var.awsprops, "secgroupname")
+  description = lookup(var.awsprops, "secgroupname")
+  vpc_id = lookup(var.awsprops, "vpc")
+  
+  ingress {
+    from_port = 22
+    protocol = "TCP"
+    to_port = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+   
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+
+resource "aws_instance" "project-iac" {
+  ami = lookup(var.awsprops, "ami")
+  instance_type = lookup(var.awsprops, "itype")
+  subnet_id = lookup(var.awsprops, "subnet") #FFXsubnet2
+  associate_public_ip_address = lookup(var.awsprops, "publicip")
+  key_name = lookup(var.awsprops, "keyname")
+
+
+  vpc_security_group_ids = [
+    aws_security_group.project-iac-sg.id
+  ]
+  root_block_device {
+    delete_on_termination = true
+    volume_size = 50
+    volume_type = "gp2"
+  }
+  tags = {
+    Name ="Testserver"
+    Environment = "DEV"
+    OS = "Linux"
+    Managed = "IAC"
+  }
+
+  depends_on = [ aws_security_group.project-iac-sg ]
+}
+
+
+output "ec2instance" {
+  value = aws_instance.project-iac.public_ip
+}
--- s3.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_s3_bucket" "bucklebuck" {
-  bucket = "bhashvika-mirna03"
-  acl    = "public"
-
-  tags = {
-    Name        = "bhashvi bala"
-    Environment = "UAT"
-  }
-}
